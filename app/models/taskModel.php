01251c3a2c7b421cd28c25ae40d47dbfa4920db0
Pablo Tulian <agustinmascheroni72@gmail.com>
2024-04-02 11:31:12 +0200

    Actualización rama read

--- app/models/taskModel.php
+++ app/models/taskModel.php
@@ -1,11 +1,18 @@
 <?php
 //namespace app\models;
-Class taskModel extends Model {
-    
-public function pruebaAction() : void {
-    echo "Estamos fritos desde el modelo";
-    
-    
-}
-}
+Class ReadModel extends Model {
+
+    protected $dbTask;
+
+    public function __construct() {
+        parent::__construct();
+        $this->dbTask = __DIR__ ."/models/dataTask.json";
+    }
+    public function readTasks() {
+         $newDb = file_get_contents($this->dbTask); //función lee el contenido de la variable que almacena el .json y lo almacena en $newDb, 
+         var_dump($newDb);
+         $dbToRead = json_decode($newDb, true); //función que convierte y decodifica el .json en un array asociativo y lo almacena en $dbToRead.
+         return $dbToRead;
+        }
+    }
 ?>    
