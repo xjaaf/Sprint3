59cf98d6d6276e086993a918d7a48637c831c027
JAF <xjaaf@icloud.com>
2024-04-02 18:26:35 +0200

    fixed date format, json permition ables

--- phpInitialDemo-main/app/controllers/ApplicationController.php
+++ phpInitialDemo-main/app/controllers/ApplicationController.php
@@ -4,21 +4,44 @@ require_once(__DIR__ . "/../models/TaskModel.php");
  * Base controller for the application.
  * Add general things in this controller.
  */
-class ApplicationController extends Controller 
+class ApplicationController extends Controller
 {
     private $tasks;
 
-public function __construct()
-{
-        $this->tasks = new TaskModel();    
-}
-public function showTasksAction(){
-    
-    $taskList = $this->tasks->showTasks();
-    $this->view->taskList = $taskList;
-}
-public function createTaskAction(){
+    public function __construct()
+    {
+        $this->tasks = new TaskModel();
+    }
+    public function showTasksAction()
+    {
 
- $this->view;
+        $taskList = $this->tasks->showTasks();
+        $this->view->taskList = $taskList;
+    }
+    public function createTaskAction()
+    {
+
+        if ($_SERVER["REQUEST_METHOD"] === "POST") {
+            $description = $this->_getParam('description');
+            $responsible = $this->_getParam('responsible');
+            $startTask = date_create($_POST['startTask'])->format('yy-m-d');
+            $status = $this->_getParam('status');
+            $endTask = date_create($_POST['endTask'])->format('yy-m-d');
+
+
+            $newTask = [
+                'id' => $this->tasks->nextId(),
+                'description' => $description,
+                'responsible' => $responsible,
+                'startTask' => $startTask,
+                'status' => $status,
+                'endTask' => $endTask,
+            ];
+       
+
+            $this->tasks->createTask($newTask);
+            header('Location: ' . $this->_baseUrl());
+            exit();
+        }
+    }
 }
-} 
--- phpInitialDemo-main/app/models/TaskModel.php
+++ phpInitialDemo-main/app/models/TaskModel.php
@@ -2,23 +2,35 @@
 
 class TaskModel extends Model{
 
-    //protected static $idTask = 1;
+    protected static $nextId = 1;
     protected array $task;
     protected $dbTask;
 
     public function __construct() {
         $this->dbTask = __DIR__ . "/dataTask.json";
         $this->task = [];
-        //$this->idTask++;
     }
 
+    public function nextId()
+    {
+        return ++self::$nextId;
+    }
     public function showTasks (){
         $dataBase = file_get_contents($this->dbTask); // saca datos de la base de datos y los guarda en variable dataBase. 
         $this->task = json_decode($dataBase, true); // nos da la info en forma de array
         
         return $this->task;
     }
+    public function createTask ($task){
+        
+        $this->showTasks();
+        $this->task[]=$task;
+        
+        $dbTask = json_encode($this->task, JSON_PRETTY_PRINT);
+        file_put_contents($this->dbTask, $dbTask);
+       
 
+    }
 
 
 }
