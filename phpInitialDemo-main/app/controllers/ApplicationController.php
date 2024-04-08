2d8f1269931cc9255a6aae7ac4ce6cf175f4d5dd
JAF <xjaaf@icloud.com>
2024-04-08 12:46:28 +0200

    read created

--- phpInitialDemo-main/app/controllers/ApplicationController.php
+++ phpInitialDemo-main/app/controllers/ApplicationController.php
@@ -1,10 +1,18 @@
 <?php
+require_once(__DIR__ . "/../models/TaskModel.php");
 
-/**
- * Base controller for the application.
- * Add general things in this controller.
- */
-class ApplicationController extends Controller 
+class ApplicationController extends Controller
 {
-	
+    private $tasks;
+
+    public function __construct()
+    {
+        $this->tasks = new TaskModel();
+    }
+
+    public function showTasksAction()
+    {
+        $taskList = $this->tasks->showTasks();
+        $this->view->taskList = $taskList;
+    }
 }
